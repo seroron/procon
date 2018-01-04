91c92d2f42c244344c8001144ba2dc4633a24084
Kenichi HIROSE <seroron@outlook.com>
2018-01-04 10:17:20 +0900

    move

---
+++ template.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <climits>
+#include <tuple>
+
+using namespace std;
+
+void solve() {
+    printf("NO\n");
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);    
+    getchar();
+
+    for(int i=0; i<n; i++) {
+        solve();
+    }
+    
+    return 0;
+}
